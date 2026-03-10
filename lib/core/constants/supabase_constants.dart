ddd875180e65d0a2c63c2b761af83deb27256133
edwardqs <ventas1@pauserdistribucionessac.onmicrosoft.com>
2026-03-10 14:10:24 -0500

    cambios en app

--- lib/core/constants/supabase_constants.dart
+++ lib/core/constants/supabase_constants.dart
@@ -1,4 +1,4 @@
 class SupabaseConstants {
-  static const String url = 'https://161.132.48.71:8443';
+  static const String url = 'http://161.132.48.71:8000';
   static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzcyODU5NjAwLCJleHAiOjE5MzA2MjYwMDB9.kRsHdYxiP9wHzNcfx5g1vM0ZbyU-XdvU6Gb2WhVzE4s';
 }
